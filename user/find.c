6ded4352e66ebb4c301159168772ad9af7498b91
36H <admin@36h.top>
2024-05-07 15:07:55 +0800

    optional challenges 1 2 finished

--- user/find.c
+++ user/find.c
@@ -4,6 +4,10 @@
 #include "kernel/fs.h"
 
 void find(char *path,char *file_name);
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int match(char *re, char *text);
 
 int main(int argc, char *argv[]){
     if(argc != 3){
@@ -38,7 +42,7 @@ void find(char *path,char *file_name){
     switch(st.type){
 	case T_FILE:
 		// 如果文件名结尾匹配 `/target`，则视为匹配
-		if(strcmp(path + strlen(path) - strlen(file_name), file_name) == 0) {
+		if(match(file_name, path)) {
 			printf("%s\n", path);
 		}
 		break;
@@ -68,4 +72,40 @@ void find(char *path,char *file_name){
 		break;
 	}
 	close(fd);
+}
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
